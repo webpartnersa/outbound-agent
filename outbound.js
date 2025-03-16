df7263ed1e3898fc0318966f9a5c9404700ba782
webpartnersa <brian@wpdev.co.za>
2025-03-16 18:17:36 +0200

    Update outbound.js

--- outbound.js
+++ outbound.js
@@ -325,7 +325,7 @@ fastify.register(async fastifyInstance => {
 });
 
 // Start the Fastify server
-fastify.listen({ port: PORT }, err => {
+fastify.listen({ port: PORT, host: "0.0.0.0" }, err => {
   if (err) {
     console.error("Error starting server:", err);
     process.exit(1);
